7a6eb41a02476a337f6fdd21426247a2184f972c
GabsFaria <gabriel-farinha@hotmail.com>
2018-10-17 10:47:15 -0300

    Ordenação de listas

--- src/br/com/bytebank/banco/test/util/TesteComparator.java
+++ src/br/com/bytebank/banco/test/util/TesteComparator.java
@@ -10,7 +10,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-public class Teste {
+public class TesteComparator {
 
     public static void main(String[] args) {
         Conta cc1 = new ContaCorrente(22, 33);
